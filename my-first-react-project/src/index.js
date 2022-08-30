aeead8248f2cec2060771c5df76e13c636b411cb
Warren <hello@warren.codes>
2022-08-30 12:23:15 -0600

    Add Hello component

--- my-first-react-project/src/index.js
+++ my-first-react-project/src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+import Hello from './components/Hello';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Hello />
   </React.StrictMode>
 );
